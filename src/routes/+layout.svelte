b42a1c722864a8ca9ea446b9be91815e583a6da3
Sam Li <weisen.li@hotmail.com>
2025-12-05 19:53:26 +0000

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Hans Construction Canada</title>
+</svelte:head>
+
+{@render children()}
