435b50dc4042b7cd193a3562149b29515f794b1e
Sam Li <weisen.li@hotmail.com>
2025-12-06 16:31:06 +0000

    add modulars data

---
+++ src/lib/modulars.ts
@@ -0,0 +1,63 @@
+export interface Modular {
+  id: string;
+  title: string;
+  images: string[];
+}
+
+export const modulars: Modular[] = [
+  {
+    id: 'home',
+    title: 'Home',
+    images: [
+      '/modulars/home/1st-and-2nd-Modular-Connection-Well.jpg',
+      '/modulars/home/1st-Hall-Way-1.jpg',
+      '/modulars/home/2nd-Bedroom-area.jpg',
+      '/modulars/home/2nd-Floor-Family-Area-1.jpg',
+      '/modulars/home/2nd-Floor-Family-room-2.jpg',
+      '/modulars/home/2nd-Floor-Washroom-and-Plumbing-HVAC-Rough-in.jpg',
+      '/modulars/home/2nd-HVAC-Rough-in-and-Connection-Well.jpg',
+      '/modulars/home/3rd-Floor-Bedroom-area-1.jpg',
+      '/modulars/home/3rd-Floor-HVAC-Rough-In-work.jpg',
+      '/modulars/home/3rd-Floor-Interior-1.jpg',
+      '/modulars/home/3rd-Floor-Interior-2.jpg',
+      '/modulars/home/3rd-Floor-Master-Bedroom-Area-1.jpg',
+      '/modulars/home/3rd-floor-Modular-with-Flat-Roof.jpg',
+      '/modulars/home/3rd-Floor-Modulars-2.jpg',
+      '/modulars/home/3rd-Floor-Roof-Joist-details.jpg',
+      '/modulars/home/3rd-floor-wahsroom-for-bedroom-and-Rough-in.jpg',
+      '/modulars/home/3rd-Floor-Washroom-and-Rough-in.jpg',
+      '/modulars/home/Back-of-TH.jpg',
+      '/modulars/home/Between-1st-and-2nd-Floor.jpg',
+      '/modulars/home/Electrical-panel-1.jpg',
+      '/modulars/home/Flat-platform-2.jpg',
+      '/modulars/home/Front-view-1.jpg',
+      '/modulars/home/IMG_5878.HEIC',
+      '/modulars/home/interior-1st-Floor-1.jpg',
+      '/modulars/home/Interior-1st-Floor-2.jpg',
+      '/modulars/home/Kitchen-and-Living-area-1st-Floor.jpg',
+      '/modulars/home/Kitchen-Area-1st-Floor.jpg',
+      '/modulars/home/Main-steel-structure-Fab-Equipment-1.jpg',
+      '/modulars/home/Main-Steel-Structure-Fab-equipment-2.jpg',
+      '/modulars/home/Main-Steel-Structure-Fab-Equipment-3.jpg',
+      '/modulars/home/Main-Steel-structure-for-Modular-1.jpg',
+      '/modulars/home/Main-Steel-Structure-for-Modular-2.jpg',
+      '/modulars/home/Mechanical-Room-1.jpg',
+      '/modulars/home/Modular-house-framing-1.jpg',
+      '/modulars/home/Modular-House-framing-2.jpg',
+      '/modulars/home/Powder-room-1st-floor.jpg',
+      '/modulars/home/Pre-2.jpg',
+      '/modulars/home/Pre-3.jpg',
+      '/modulars/home/Pre-4.jpg',
+      '/modulars/home/Pre-5.jpg',
+      '/modulars/home/Pre-6.jpg',
+      '/modulars/home/Pre-7.jpg',
+      '/modulars/home/Pre-8.jpg',
+      '/modulars/home/Pre-9.jpg',
+      '/modulars/home/Pre-stack-1.jpg',
+      '/modulars/home/Side-view-1.jpg',
+      '/modulars/home/Side-View-2.jpg',
+      '/modulars/home/Steel-End-Framing-Fab-Platform-1.jpg',
+      '/modulars/home/Steel-Framings-1.jpg'
+    ]
+  }
+];
