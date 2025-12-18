e3bfa32d4d82bfe9d6c50c349c04e7eac2da6a27
Sam Li <weisen.li@hotmail.com>
2025-12-18 18:58:04 +0000

    upgrade tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#fef2f2',
+          100: '#fee2e2',
+          400: '#f87171',
+          500: '#ef4444',
+          600: '#dc2626',
+          700: '#b91c1c',
+          900: '#7f1d1d'
+        }
+      }
+    },
   },
   plugins: [],
 }
