d7fc0417cfc47bf389d6dd0d545881df21f4ad34
Sam Li <weisen.li@hotmail.com>
2026-01-11 22:11:36 +0000

    remove number from address

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,24 +1,24 @@
 export const projects = [
   {
-    id: '113-dexter-rd-richmond-hill',
-    title: '113 Dexter Rd, Richmond Hill',
-    images: Array.from({ length: 51 }, (_, i) => `/projects/113-dexter-rd-richmond-hill/image-${String(i + 1).padStart(3, '0')}.jpg`)
+    id: 'dexter-rd-richmond-hill',
+    title: 'Dexter Rd, Richmond Hill',
+    images: Array.from({ length: 51 }, (_, i) => `/projects/dexter-rd-richmond-hill/image-${String(i + 1).padStart(3, '0')}.jpg`)
   },
   {
-    id: '36-honey-locust-vaughan',
-    title: '36 Honey Locust, Vaughan',
+    id: 'honey-locust-vaughan',
+    title: 'Honey Locust, Vaughan',
     images: [
-      '/projects/36-honey-locust-vaughan/image-001.heic',
-      '/projects/36-honey-locust-vaughan/image-002.heic'
+      '/projects/honey-locust-vaughan/image-001.heic',
+      '/projects/honey-locust-vaughan/image-002.heic'
     ]
   },
   {
-    id: '36-kensington-dr-richmond-hill',
-    title: '36 Kensington Dr, Richmond Hill',
+    id: 'kensington-dr-richmond-hill',
+    title: 'Kensington Dr, Richmond Hill',
     images: Array.from({ length: 52 }, (_, i) => {
       const num = String(i + 1).padStart(3, '0');
       const ext = i < 12 || (i >= 14 && i <= 27) || i >= 36 ? 'jpg' : 'heic';
-      return `/projects/36-kensington-dr-richmond-hill/image-${num}.${ext}`;
+      return `/projects/kensington-dr-richmond-hill/image-${num}.${ext}`;
     })
   },
   {
